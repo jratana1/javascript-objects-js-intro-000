f907413d5c1a40ed5807cfeb891ce3a82eaeeb9f
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-26 23:22:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {BobDylan:'Dont think twice'}
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[`${artistName}`] = songTitle;
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[`${artistName}`]
+}
